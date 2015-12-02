e0420f4361dc9d6d22580bd436df48acb223d056
rafal2208 <puma2208@wp.pl>
2015-12-02 10:50:44 +0100

    add comments

--- test/bootstrap/functional.php
+++ test/bootstrap/functional.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-// guess current application
+// guess current application my comment
 if (!isset($app))
 {
   $traces = debug_backtrace();
